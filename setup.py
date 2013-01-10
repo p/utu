c4dbe8e601cb347d844fe822b6e603bd8f8f11b8
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-10 06:06:05 -0500

    Add a setup.py for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='utu',
+    version='0.1',
+    description='Underscore style for unittest module',
+    author='Oleg Pudeyev',
+    author_email='oleg@bsdpower.com',
+    url='http://github.com/p/utu',
+    py_modules=['utu'],
+)
